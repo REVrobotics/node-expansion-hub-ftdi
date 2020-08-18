486034baf875c1858eec54a67f434a019729ada0
NoahAndrews <noah@revrobotics.com>
2020-08-18 12:28:59 -0500

    Improve error handling

--- dist/binding.js
+++ dist/binding.js
@@ -3,6 +3,17 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.enterExpansionHubFirmwareUpdateMode = void 0;
 var util_1 = require("util");
 exports.enterExpansionHubFirmwareUpdateMode = function (serial) {
-    var addon = require('bindings')('addon.node');
+    var addon;
+    try {
+        addon = require('bindings')('addon.node');
+    }
+    catch (e) {
+        if (e instanceof Error && e.message.includes("module could not be found")) {
+            throw new Error("The FTDI driver is not installed correctly");
+        }
+        else {
+            throw e;
+        }
+    }
     return util_1.promisify(addon.enterExpansionHubFirmwareUpdateMode);
 };
--- lib/binding.ts
+++ lib/binding.ts
@@ -1,6 +1,15 @@
 import {promisify} from "util";
 
 export const enterExpansionHubFirmwareUpdateMode = (serial: string): Promise<void> => {
-    const addon = require('bindings')('addon.node');
+    let addon;
+    try {
+        addon = require('bindings')('addon.node');
+    } catch (e) {
+        if (e instanceof Error && e.message.includes("module could not be found")) {
+            throw new Error("The FTDI driver is not installed correctly");
+        } else {
+            throw e;
+        }
+    }
     return (promisify(addon.enterExpansionHubFirmwareUpdateMode) as unknown) as Promise<void>;
 }
