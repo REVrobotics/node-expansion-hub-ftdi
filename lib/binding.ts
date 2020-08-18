5ea04d1e872a91af790f0ef956defca02d282b5b
NoahAndrews <noah@revrobotics.com>
2020-08-18 13:45:54 -0500

    Fix native code execution

--- lib/binding.ts
+++ lib/binding.ts
@@ -1,9 +1,16 @@
-import {promisify} from "util";
-
-export const enterExpansionHubFirmwareUpdateMode = (serial: string): Promise<void> => {
-    let addon;
+export const enterExpansionHubFirmwareUpdateMode = async (serial: string): Promise<void> => {
+    let addon: any;
     try {
         addon = require('bindings')('addon.node');
+        return new Promise((resolve, reject) => {
+            addon.enterExpansionHubFirmwareUpdateMode(serial, (err: Error|undefined) => {{
+                if (err) {
+                    reject(err);
+                } else {
+                    resolve();
+                }
+            }});
+        });
     } catch (e) {
         if (e instanceof Error && e.message.includes("module could not be found")) {
             throw new Error("The FTDI driver is not installed correctly");
@@ -11,5 +18,4 @@ export const enterExpansionHubFirmwareUpdateMode = (serial: string): Promise<voi
             throw e;
         }
     }
-    return (promisify(addon.enterExpansionHubFirmwareUpdateMode) as unknown) as Promise<void>;
 }
