044b36bc2ba0849b8a23a24a262f3fce5e5d0720
NoahAndrews <noah@revrobotics.com>
2021-11-28 17:26:15 -0600

    Update dependencies

--- dist/binding.js
+++ dist/binding.js
@@ -3,5 +3,5 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.enterExpansionHubFirmwareUpdateMode = void 0;
 var util_1 = require("util");
 var addon = require('bindings')('addon.node');
-exports.enterExpansionHubFirmwareUpdateMode = util_1.promisify(addon.enterExpansionHubFirmwareUpdateMode);
+exports.enterExpansionHubFirmwareUpdateMode = (0, util_1.promisify)(addon.enterExpansionHubFirmwareUpdateMode);
 // exports.enterExpansionHubFirmwareUpdateMode = enterExpansionHubFirmwareUpdateMode;
